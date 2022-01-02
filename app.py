017bf0c40d015149528d0776f78fbfb53b0172de
Soyoung Sun <dkssud02@gmail.com>
2022-01-02 16:41:05 +0900

    html 만들어보기 테스트

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World!'
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run('0.0.0.0', debug=True, port=5000)
